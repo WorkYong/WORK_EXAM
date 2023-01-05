9117019ced425bbc03854651dccf4bfbd4ace83d
WorkYong <pjyong01@gmail.com>
2023-01-05 11:47:06 +0900

    modified: modeling

--- accountbookrecords/models.py
+++ accountbookrecords/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-from accountbooks.models import AccountBook
+from .models import AccountBook , User
 from core.models         import TimeStampModel
 
 class AccountBookRecord(TimeStampModel): 
-    title        = models.CharField(max_length=100)
-    date         = models.CharField(max_length=150)
-    memo         = models.CharField(max_length=200)
-    description  = models.CharField(max_length=300)
-    amount       = models.DecimalField(decimal_places=3, max_digits=15)
-    balance      = models.DecimalField(decimal_places=3, max_digits=15)
+    title        = models.CharField(max_length = 100)
+    date         = models.CharField(max_length = 150)
+    memo         = models.CharField(max_length = 200)
+    description  = models.CharField(max_length = 300)
+    amount       = models.CharField(max_length = 200)
+    balance      = models.CharField(max_length = 200)
     is_deleted   = models.BooleanField(default = True)
     account_book = models.ForeignKey(AccountBook, on_delete = models.CASCADE)
+    user         = models.ForeignKey(User, on_delete = models.CASCADE)
     deleted_at   = models.DateTimeField(null = True, default = None)
 
     
