fbca91a0be552bac6570a95dad79854b4138a56d
WorkYong <pjyong01@gmail.com>
2023-01-07 03:20:57 +0900

    ADD : accountBookRecordCopy

--- accountbookrecords/urls.py
+++ accountbookrecords/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import AccountBookRecordView
+from .views import AccountBookRecordView, AccountBookRecordDataCopyView
 urlpatterns = [
     path('', AccountBookRecordView.as_view()),
+    path('/copy', AccountBookRecordDataCopyView.as_view())
 ]
